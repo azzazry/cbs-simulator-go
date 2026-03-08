465720e426ede1b1f026a0a98066c9da0438577d
Aaz Zazri Nugraha <azzazry120@gmail.com>
2026-03-08 08:18:17 +0700

    frecator: Change character

--- utils/startup.go
+++ utils/startup.go
@@ -12,10 +12,10 @@ func StartupScreen(addr string) {
 	fmt.Printf("Build: %s\n", version.Build)
 	fmt.Println("")
 
-	fmt.Println("✔ Config loaded")
-	fmt.Println("✔ Database connected")
-	fmt.Println("✔ Migrations completed")
-	fmt.Println("✔ Router initialized")
+	fmt.Println("- Config loaded")
+	fmt.Println("- Database connected")
+	fmt.Println("- Migrations completed")
+	fmt.Println("- Router initialized")
 	fmt.Println("")
 
 	fmt.Printf("Server running on : %s\n", addr)
